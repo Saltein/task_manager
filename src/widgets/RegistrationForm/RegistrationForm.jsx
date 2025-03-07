48c6b1059fd2e4ed79435afd2439f78a180d24c7
Никита <nikitval@list.ru>
2025-03-07 15:45:35 +0300

    форма регистрации

--- src/widgets/RegistrationForm/RegistrationForm.jsx
+++ src/widgets/RegistrationForm/RegistrationForm.jsx
@@ -1,14 +1,32 @@
-import { DefaultButton, DefaultInput } from "../../shared"
-import s from "./RegistrationForm.module.css"
+import { useState } from "react";
+import { DefaultButton, DefaultInput } from "../../shared";
+import s from "./RegistrationForm.module.css";
 
 export const RegistrationForm = (props) => {
+    const [isRegister, setIsRegister] = useState(false);
+
+    const handleToggle = () => {
+        setIsRegister((prev) => !prev);
+    };
+
     return (
         <div className={s.r_form}>
+            <div className={s.logo_label}>
+                <img src='/icons/tomato.svg' className={s.logo_img} alt="Logo" />
+                <span className={s.label}>Pomodoro PLAN</span>
+            </div>
+
             <div className={s.inputs}>
+                <div className={`${s.animated_field} ${isRegister ? s.show : ""}`}>
+                    <DefaultInput type='text' placeholder='Имя' />
+                </div>
                 <DefaultInput type='text' placeholder='Email' />
                 <DefaultInput type='password' placeholder='Пароль' />
+                <DefaultButton label={isRegister ? "ЗАРЕГИСТРИРОВАТЬСЯ" : "ВОЙТИ"} />
+                <a className={s.reg_butt} onClick={handleToggle}>
+                    {isRegister ? "Войти" : "Зарегистрироваться"}
+                </a>
             </div>
-            <DefaultButton label="ЖОПА" />
         </div>
-    )
-}
+    );
+};
