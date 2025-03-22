8fa95870a6de6204802fc375e1a1997d157fd2c1
1DamnDaniel3 <swagawpkek@gmail.com>
2025-03-22 16:53:32 +0300

    Configure MSW

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
-import { worker } from './shared/mocks/browser';
 
-import {API_URL} from './shared/config/api'
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
 
-if (process.env.NODE_ENV === 'development'){
-  worker.listen()
-  async function app() {
-    const response = await fetch(`${API_URL}/tasks`)
-    const task = await response.json()
-    console.log(task)
+  try {
+    const { worker } = await import('./shared/mocks/browser');
     
+    await worker.start({
+      onUnhandledRequest: 'warn', // Логируем все неизвестные запросы
+    });
+    
+  } catch (error) {
+    console.error('MSW не смог запуститься', error);
   }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}) 
--- src/shared/mocks/browser.js
+++ src/shared/mocks/browser.js
@@ -1,5 +1,8 @@
 import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
+
 export const worker = setupWorker(...handlers);
-worker.start();
+
+
+
