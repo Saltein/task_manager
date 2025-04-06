48cdbd4e56a42078108d2d245772b26bf6d475f2
Никита <nikitval@list.ru>
2025-04-06 16:10:02 +0300

    current paths in user/api/api.js

--- src/entities/user/api/api.js
+++ src/entities/user/api/api.js
@@ -1,6 +1,6 @@
 import api from '../../../shared/api/base'
 
 export const userAPI = {
-    login: async (data) => api.post('/login', data),
-    registration: async (data) => api.post('/registration', data),
+    login: async (data) => api.post('/auth/login', data),
+    registration: async (data) => api.post('/auth/registration', data),
 }
