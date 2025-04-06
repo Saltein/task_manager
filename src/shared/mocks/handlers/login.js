f73fd6fbdbd1f9daab6c9fe3dc2a678c8f2c2fec
Никита <nikitval@list.ru>
2025-04-06 18:15:00 +0300

    edfgdfgdfgdfg

--- src/shared/mocks/handlers/login.js
+++ src/shared/mocks/handlers/login.js
@@ -2,7 +2,7 @@ import {http, HttpResponse} from 'msw'
 import { API_URL } from '../../config/api'
 
 export const loginHandlers = [
-    http.post(`${API_URL}/login`, async ({ request }) => {
+    http.post(`${API_URL}/auth/login`, async ({ request }) => {
         const { email, password } = await request.json();
         if (email === 'Admin@gmail.com' && password === 'AdminAdmin!'){
             return HttpResponse.json({message: 'Authorisation success!'}, {status: 200});
--- src/shared/mocks/handlers/registration.js
+++ src/shared/mocks/handlers/registration.js
@@ -3,7 +3,7 @@ import { API_URL } from '../../config/api'
 
 
 export const registrationHandlers = [
-    http.post(`${API_URL}/registration`, async ({ request }) => {
+    http.post(`${API_URL}/auth/registration`, async ({ request }) => {
         const {email, password, name} = request.json();
         if(email !== "Admin@gmail.com" && password !== "Admin" && name !== "AdminAdmin!"){
             return HttpResponse.json({message: "Registration successful!"}, {status: 200});
