a69b9eb24b6e074f329d28406780846ef36b90c0
Никита <87192441+Saltein@users.noreply.github.com>
2025-04-07 16:24:24 +0300

    a

--- src/entities/task/api/api.js
+++ src/entities/task/api/api.js
@@ -2,6 +2,6 @@ import api from '../../../shared/api/base'
 
 export const taskAPI = {
     getTasks: async (data) => api.get('/tasks', data),
-    addTask: async (data) => api.put('/tasks/:id', data),
+    addTask: async (data) => api.post('/tasks', data),
     deleteTask: async (data) => api.delete('/tasks/:id', data)
 }
